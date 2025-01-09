8eae32030ecfb211f1ffc19734580c7ef377a1b7
MohamedKhX <muhamedkhx2@gmail.com>
2025-01-09 12:14:08 +0200

    add

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use Filament\Forms\Form;
+use Filament\Tables\Columns\Column;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +24,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
+        Table::configureUsing(function (Table $table) {
+            $table->defaultSort('created_at', 'desc')->striped();
+        });
+
+        Form::configureUsing(function (Form $form) {
+            $form->extraAttributes([
+                'lang' => 'en'
+            ]);
+        });
+
+        TextColumn::configureUsing(function (Column $column) {
+            $column->extraAttributes([
+                'lang' => 'en'
+            ]);
+        });
     }
 }
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 100);
+            $table->string('email', 100)->unique();
             $table->enum('type', \App\Enums\UserTypeEnum::values())
                 ->default(UserTypeEnum::Trader);
 
